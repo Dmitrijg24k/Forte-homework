210cd597edd078022c7ddf3c4510cbc035390582
Dmitrijg24k <dmitrij3e5g@gmail.com>
2022-02-27 17:00:39 +0400

    output config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import join from 'lodash/join';
 import './style.css';
 import Icon from './icon.png';
+import printMe from './print.js';
 
 function test() {
     const element = document.createElement('div');
+
     element.innerHTML = join(['Hello', 'webpack'], ' ');
 
     element.classList.add('hello');
+
+    const btn = document.createElement('button');
+    btn.innerHTML = 'Click me';
+    btn.onclick = printMe;
+  
+    element.appendChild(btn);
 
     const myIcon = new Image();
     myIcon.src = Icon;
